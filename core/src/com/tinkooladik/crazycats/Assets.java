7ae06f93f8989bb5a03dd1cdc72a0f1811054b6c
tinkooladik <qwertyuiop1A>
2017-01-25 11:17:29 +0200

    fixed some pics

--- core/src/com/tinkooladik/crazycats/Assets.java
+++ core/src/com/tinkooladik/crazycats/Assets.java
@@ -8,39 +8,39 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Assets {
-	
-	private static TextureAtlas catsAtlas;
-	public static TextureAtlas items;
-	public static TextureRegion dialog_back, dialog_bg, dialog_btn, dialog_btnPassive, transp_btn;
-	public static TextureRegion fish, life, lostLife, pause, pause_back, pauseBg, help, touch;
-	public static TextureRegion musicOff, audio, settings, leaderboard;
-	public static TextureRegion replay, menu;
-	public static TextureRegion progressBar, knob;
-	public static Texture plus5, plusLife;
-	public static Music musicPlay;
-	public static Sound soundPlay;
-	
-	static void load() {
-		loadCatsAtlas();
+
+    private static TextureAtlas catsAtlas;
+    public static TextureAtlas items;
+    public static TextureRegion dialog_back, dialog_bg, dialog_btn, dialog_btnPassive, transp_btn;
+    public static TextureRegion fish, life, lostLife, pause, pause_back, pauseBg, help, touch;
+    public static TextureRegion musicOff, audio, settings, leaderboard;
+    public static TextureRegion replay, menu;
+    public static TextureRegion progressBar, knob;
+    public static Texture plus5, plusLife;
+    public static Music musicPlay;
+    public static Sound soundPlay;
+
+    static void load() {
+        loadCatsAtlas();
         items = new TextureAtlas("data/items.pack");
-        
+
         dialog_back = items.findRegion("dialog_back");
         dialog_bg = items.findRegion("dialog_bg");
         dialog_btn = items.findRegion("dialog_btn");
         dialog_btnPassive = items.findRegion("dialog_btnPassive");
         transp_btn = items.findRegion("transp_btn");
-        
+
         fish = items.findRegion("fish");
         life = items.findRegion("life");
         lostLife = items.findRegion("lostLife");
         touch = items.findRegion("touch");
         plus5 = new Texture("data/p5.png");
-		plusLife = new Texture("data/pLife.png");
-        
+        plusLife = new Texture("data/pLife.png");
+
         pause = items.findRegion("pause");
         pause_back = items.findRegion("pause_back");
         pauseBg = items.findRegion("pauseBg");
-        
+
         menu = items.findRegion("menu");
         replay = items.findRegion("replay");
 
@@ -48,27 +48,27 @@ public class Assets {
         audio = items.findRegion("audio");
         musicOff = items.findRegion("off");
         leaderboard = items.findRegion("leaderboard");
-        
-        help = new TextureRegion(new Texture("data/help.png"));
-        
-        musicPlay = Gdx.audio.newMusic(Gdx.files.internal("data/music.mp3"));
-		musicPlay.setLooping(true);
-		musicPlay.setVolume(1.0f);
-		if(Settings.musicEnabled) musicPlay.play();
-		
-		soundPlay = Gdx.audio.newSound(Gdx.files.internal("data/sound.mp3"));
 
-		progressBar = new TextureRegion(new Texture("data/pb.png"));
-		knob = new TextureRegion(new Texture("data/knob.png"));
-	}
-	
-	public static TextureRegion getCat(int i) {
-		return catsAtlas.findRegion("cat"+i);
-	}
-	
-	public static void loadCatsAtlas() {
-		/*
-		 * switch(Settings.catsPack) {
+        help = new TextureRegion(new Texture("data/help.png"));
+
+        musicPlay = Gdx.audio.newMusic(Gdx.files.internal("data/music.mp3"));
+        musicPlay.setLooping(true);
+        musicPlay.setVolume(1.0f);
+        if (Settings.musicEnabled) musicPlay.play();
+
+        soundPlay = Gdx.audio.newSound(Gdx.files.internal("data/sound.mp3"));
+
+        progressBar = new TextureRegion(new Texture("data/pb.png"));
+        knob = new TextureRegion(new Texture("data/knob.png"));
+    }
+
+    public static TextureRegion getCat(int i) {
+        return catsAtlas.findRegion("cat" + i);
+    }
+
+    public static void loadCatsAtlas() {
+        /*
+         * switch(Settings.catsPack) {
 		case 0: 
 	        catsAtlas = new TextureAtlas("data/cats.pack");
 	        break;
@@ -76,7 +76,7 @@ public class Assets {
 	        catsAtlas = new TextureAtlas("data/cats_alive.pack");
 	        break;
 		}*/
-		catsAtlas = new TextureAtlas("data/aliveCats.pack");
-	}
+        catsAtlas = new TextureAtlas("data/aliveCats.pack");
+    }
 
 }
