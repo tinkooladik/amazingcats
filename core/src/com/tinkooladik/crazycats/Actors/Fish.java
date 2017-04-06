8a16fc2ec41f0cc5adfda4986c6e8ced229e6f9e
tinkooladik <tinkooladik@gmail.com>
2017-04-06 15:52:19 +0300

    removing fishes

--- core/src/com/tinkooladik/crazycats/Actors/Fish.java
+++ core/src/com/tinkooladik/crazycats/Actors/Fish.java
@@ -13,9 +13,11 @@ import com.tinkooladik.crazycats.Settings;
 public class Fish extends Actor {
 
   private AcidCat game;
+  private int lifecycle;
 
   public Fish(AcidCat game, float x, float y) {
     this.game = game;
+    lifecycle = 550;
     setSize(Gdx.graphics.getWidth() / 5, Gdx.graphics.getWidth() / 5 - 40);
     setPosition(x, y);
     addListener(new EnterFishListener());
@@ -25,6 +27,14 @@ public class Fish extends Actor {
     batch.setColor(getColor());
     batch.draw(AcidCat.manager.get(Assets.txrFish, Texture.class), getX(), getY(), getWidth(),
         getHeight());
+  }
+
+  @Override public void act(float delta) {
+    if (lifecycle > 0) {
+      lifecycle--;
+    } else {
+      remove();
+    }
   }
 
   private class EnterFishListener extends InputListener {
